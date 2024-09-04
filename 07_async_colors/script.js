6ddf9dea364cb699d4d1fadfd1f2d7c96906c538
adil <adilahmed3886@gmail.com>
2024-09-04 21:46:22 +0500

    update

--- 07_async_colors/script.js
+++ 07_async_colors/script.js
@@ -7,6 +7,18 @@ const h1 = document.querySelector("h1");
 let intervalId;
 
 function changeColor(){
+
+// ANOTHER METHOD, WILL WORK ON THIS AGAIN
+//     let color = "#";
+//   let hex = "0123456789ABCDEF";
+//   for (let index = 0; index < 6; index++) {
+//     color += hex[Math.floor(Math.random() * 16)];
+//   }
+//   body.style.backgroundColor = color
+//   div.style.backgroundColor = `#${Math.floor(Math.random() * 999999) + 100000}`;
+//   h1.style.color = `#${Math.floor(Math.random() * 999999) + 100000}`;
+
+
     body.style.backgroundColor = `#${Math.floor(Math.random() * 999999) + 100000}`;
     div.style.backgroundColor = `#${Math.floor(Math.random() * 999999) + 100000}`;
     h1.style.color = `#${Math.floor(Math.random() * 999999) + 100000}`;
@@ -26,5 +38,3 @@ stop.addEventListener("click", function(){
     }
 })
 
- 
-console.log(`#${Math.floor(Math.random() * 999999) + 100000}`)
