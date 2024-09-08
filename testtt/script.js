4bddaac225c34233c443bea4b79c1f74fd289561
adil <adilahmed3886@gmail.com>
2024-09-09 00:13:08 +0500

    practicing

--- testtt/script.js
+++ testtt/script.js
@@ -32,8 +32,41 @@
 //     console.log(error);
 // })
 
-let array = [1,2,3,4,5];
-newArray = Array.from(array)
-console.log(typeof newArray);
+// let array = [1,2,3,4,5];
+// newArray = Array.from(array)
+// console.log(typeof newArray);
+
+
+// const generateHashTag = (str) => {
+//     if(str.trim().length > 280 || str.trim().length === 0){
+//         return false
+//     }else{
+//         newStr = str.split(" ")
+//         newNewStr = newStr.map( (curElem) => {
+//             return curElem.replace(curElem[0], curElem[0].toUpperCase())
+//         })
+//         strJoin = newNewStr.join("");
+//         endResult = `#${strJoin}`
+//         console.log(endResult)
+        
+//     }
+// }
+
+
+// const generateHashTag = (str) => {
+//     let hashTag = "#";
+//     for (const word of str.split(" ")) {
+//         if(word){
+//             hashTag += word.charAt(0).toUpperCase() + word.slice(1);
+//         }
+        
+//     }
+//     return hashTag;
+// }
+
+
+// console.log(generateHashTag("hi im a dev"));
+
+// [1, 2, 3].forEach(num => console.log(num));
 
 
